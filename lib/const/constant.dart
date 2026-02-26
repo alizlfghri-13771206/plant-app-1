25d521bbc33978f205764da237f1c7cb91227d79
alizlfghri-13771206 <ali.zlfghri@gmail.com>
2026-02-26 21:32:25 +0330

    create constant class

---
+++ lib/const/constant.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class Constant {
+  static Color primaryColor = Colors.green;
+  static Color blackColor = Colors.black;
+
+  static String titleOne = "گیاهان را بهتر از قبل درک کن";
+  static String descriptionOne =
+      " در مورد نگه‌داری گل و گیاهان می‌توانی اطلاعات کسب کنی ";
+
+  static String titleTwo = "با گیاهان جدید آشنا شو";
+  static String descriptionTwo =
+      "رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی";
+
+  static String titleThree = "با یک گل بهار نمی‌شود،گل بکار";
+  static String descriptionThree = "هر گلی نیاز باشد در این اگ هست";
+}
